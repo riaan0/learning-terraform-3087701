2d61858cdd407eb5696b41bcae144eba63ad6730
riaan0 <139359335+riaan0@users.noreply.github.com>
2023-07-14 15:42:04 +0100

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "${var.environment.name}-blog"
+  name     = "${var.environment.name}-blog"
   min_size = var.asg_min_size
   max_size = var.asg_max_size
 
--- variables.tf
+++ variables.tf
@@ -33,10 +33,10 @@ variable "environment" {
 
 variable "asg_min_size" {
   description  = "Minimum number of instances in the ASG"
-  default.     = "1"
+  default.     = 1
 }
 variable "asg_max_size" {
   description  = "Maximum number of instances in the ASG"
-  default.     = "2"
+  default.     = 2
 }
 
