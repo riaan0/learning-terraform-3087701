388193d07d95d75ae06158c2855d5c26ef556f0a
riaan0 <139359335+riaan0@users.noreply.github.com>
2023-07-14 14:54:30 +0100

    vpc_id fix

--- main.tf
+++ main.tf
@@ -85,9 +85,8 @@ module "blog_sg" {
   source   = "terraform-aws-modules/security-group/aws"
   version  = "4.13.0"
 
-  name     = "blog"
-  vpc_id   = module.vpc.public_subnets[0]
-
+  name                = "blog"
+  vpc_id.             = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
