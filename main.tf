7266e5e73520cb8dd2feb8fefddbdf6ea0a0ad20
riaan0 <139359335+riaan0@users.noreply.github.com>
2023-07-14 13:10:34 +0100

    Chnage VPC

--- main.tf
+++ main.tf
@@ -45,13 +45,12 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source   = "terraform-aws-modules/security-group/aws"
   version  = "5.1.0"
-  name     = "blog"
 
+  name     = "blog"
   vpc_id   = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
