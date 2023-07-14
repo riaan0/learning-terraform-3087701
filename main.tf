87549f5978a51bcb85a74c510ba8e2ccd18db880
riaan0 <139359335+riaan0@users.noreply.github.com>
2023-07-14 14:55:56 +0100

    argument added

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ module "blog_sg" {
   version  = "4.13.0"
 
   name                = "blog"
-  vpc_id.             = module.blog_vpc.vpc_id
+  vpc_id.             = "module.blog_vpc.vpc_id"
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
